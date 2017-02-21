20f857978493c14d432a3dbf91fb26d2d1423c21
Steven BOEHM <boehm_s@etna-alternance.net>
2017-02-21 11:47:33 +0100

    less and less jquery ...

--- website/assets/js/add.js
+++ website/assets/js/add.js
@@ -1,76 +1,98 @@
+var INGREDIENTS = document.getElementById('ingredients');
+var CATEGORY = document.getElementById('category');
+var INSTRUCTION = document.getElementById('instruction');
+var PICTURE = document.getElementById('picture');
+var NBR = document.getElementById('nbr');
+var NAME = document.getElementById('name');
+var C_plus = document.getElementsByClassName('plus');
+var C_remove = document.getElementsByClassName('remove');
+var C_ingredients = INGREDIENTS.getElementsByClassName('ingredients');
+var T_select = document.getElementsByTagName('select');
+
 // AFFICHER LES CATEGORIES
-$.get( "../API/index.php?controller=recettes&action=getCategory", function( data ) {
-  var data = JSON.parse(data);
-  for (var i = 0; i < data.length; i++) {
-    $("#category").append(`<option value="${data[i].id}">${data[i].name}</option>`);
-  }
-  $('select').material_select();
+xhrRequest.GET({url: "../API/index.php?controller=recettes&action=getCategory"}, function(res) {
+    var data = JSON.parse(res.responseText);
+    data.forEach(function(optInfo) {
+	var opt = document.createElement('option');
+	opt.value = optInfo.id;
+	opt.innerHTML = optInfo.name;
+	CATEGORY.appendChild(opt);
+    });
+    Array.from(T_select).forEach(function(select) {
+    	$(select).material_select();
+    });
 });
-$(document).ready(function() {
 
-  $('.plus').click(function(){
-    $('#ingredients ul').append(`
-      <li class="collection-item row ingredients">
-      <input placeholder="nom" class="col s5 name">
-      <input placeholder="quantitee" class="col s2 amount">
-      <input placeholder="unite" class="col s3 unit">
-      <i class="material-icons col s1"></i>
-      <i class="material-icons col s1" onclick="this.parentNode.remove()">delete</i>
-      </li>
-      `);
-    })
-    $('.remove').click(function(){
-      $(this).parent().parent().remove();
-    })
-  });
+document.addEventListener('DOMContentLoaded', function(e) {
+    e.preventDefault();
+    e.stopPropagation();
 
-  function buildRecette() {
-    let ret = {}
+    Array.from(C_plus).forEach(function(plus) {
+	plus.onclick = function(e){
+	    e.preventDefault();
+	    e.stopPropagation();
+	    Array.from(INGREDIENTS.getElementsByTagName('ul')).forEach(function(ul) {
+		var div = document.createElement('div');
+		div.innerHTML = [
+		    '<li class="collection-item row ingredients">',
+		    '<input placeholder="nom" class="col s5 name">',
+		    '<input placeholder="quantitee" class="col s2 amount">',
+		    '<input placeholder="unite" class="col s3 unit">',
+		    '<i class="material-icons col s1"></i>',
+		    '<i class="material-icons col s1 delete" onclick="this.parentNode.remove()">delete</i>',
+		    '</li>'
+		].join('');
+		ul.appendChild(div);
+	    });
+	};
+    });
+
+    Array.from(C_remove).forEach(function(remove) {
+	remove.onclick = function(e){
+	    e.preventDefault();
+	    e.stopPropagation();
+	    this.parentNode.parentNode.remove();
+	};
+    });
+});
+
+function buildRecette() {
+    var ret = {};
     // GET INGREDIENTS
-    let ingredients = [];
-    $("#ingredients .ingredients").map((v,w) => {
-      let name = $(w).find('.name').val();
-      let amount = $(w).find('.amount').val();
-      let unit = $(w).find('.unit').val();
-      ingredients.push({name, amount, unit})
-    })
+    var ingredients = Array.from(C_ingredients).map(function(ingredient) {
+	var name = ingredient.getElementsByClassName('name')[0].value;
+	var amount = ingredient.getElementsByClassName('amount')[0].value;
+	var unit = ingredient.getElementsByClassName('unit')[0].value;
+
+    	return {name: name, amount: amount, unit: unit};
+    });
 
     // GET RECETTE
-    let name = $('#name').val();
-    let nbrPersonne = parseInt($('#nbr').val());
-    let category = parseInt($('#category').val());
-    let instruction = $('#instruction').val();
-    let recette = {name, nbrPersonne, category, instruction};
-
+    var name = NAME.value;
+    var nbrPersonne = parseInt(NBR.value);
+    var category = parseInt(CATEGORY.value);
+    var instruction = INSTRUCTION.value;
+    var recette = {name: name, nbrPersonne: nbrPersonne, category: category, instruction: instruction};
 
     ret.ingredients = ingredients;
     ret.recette = recette;
+
     return ret;
-  }
+}
 
-  // function create() {
-  //
-  //   var formData = new FormData($("#file"));
-  //   console.log(formData);
-  //   $.post( "http://localhost/marmiton/index.php?controller=recettes&action=create", { recette: JSON.stringify(buildRecette()) }, function( data ) {
-  //     console.log(data);
-  //   });
-  // }
-
-
-  function create() {
+function create() {
     var data = new FormData();
-    data.append("file_upload", $('#picture')[0].files[0]);
+    data.append("file_upload", PICTURE.files[0]);
     data.append("recette", JSON.stringify(buildRecette()));
 
     $.ajax({
-      url: "../API/index.php?controller=recettes&action=upload",
-      type: "POST",
-      data: data,
-      processData: false,
-      contentType: false,
-      success: function (data) {
-          Materialize.toast('Recette enregistree avec success !', 4000);
-      }
+	url: "../API/index.php?controller=recettes&action=upload",
+	type: "POST",
+	data: data,
+	processData: false,
+	contentType: false,
+	success: function (data) {
+            Materialize.toast('Recette enregistree avec success !', 4000);
+	}
     });
-  }
+}
--- website/assets/js/main.js
+++ website/assets/js/main.js
@@ -58,46 +58,37 @@ var xhrRequest = {
 var SIDEBAR = document.getElementById('sidebar');
 var SEARCH = document.getElementById('autocompleteState');
 var LOADER = document.getElementById('loader');
+var BEST = document.getElementById('best');
+var CATEGORIES = document.getElementById('categories');
 var C_autocompleteOption = document.getElementsByClassName('autocomplete-option');
 var C_autocompleteContent = document.getElementsByClassName('autocomplete-content');
 
 
 
 function bests() {
-  for (var i = 0; i < 3; i++) {
-    $('#best').append(`
-      <div class="col s4">
-      <div class="card horizontal">
-      <div class="card-image">
-      <img src="http://lorempixel.com/100/190/nature/7">
-      </div>
-      <div class="card-stacked">
-      <div class="card-content">
-      <p>I am a very simple card. I am good at containing small bits of information.</p>
-      </div>
-      <div class="card-action">
-      <a href="#">This is a link</a>
-      </div>
-      </div>
-      </div>
-      </div>`);
-  }
+    var fakeBest = '<div class="col s4"><div class="card horizontal"> <div class="card-image"> <img src="http://lorempixel.com/100/190/nature/7"> </div> <div class="card-stacked"> <div class="card-content"> <p>I am a very simple card. I am good at containing small bits of information.</p> </div> <div class="card-action"> <a href="#">This is a link</a> </div> </div> </div> </div>';
+
+    Array(3).fill().forEach(function(_, i) {
+	BEST.innerHTML += fakeBest;
+    });
 }
 
 function categories(array) {
-    $('#categories').empty();
-    for (var i = 0; i < array.length; i++) {
-	$('#categories').append(`
-      <div class="col s12 m3">
-      <div class="card">
-      <div class="card-image">
-      <img src="${array[i].picture}">
-      <div class="card-action">
-      <a href="#">${array[i].name}</a>
-      </div>
-      </div>
-      </div>`);
-    }
-}
+    CATEGORIES.innerHTML = "";
+    array.forEach(function(el) {
+	CATEGORIES.innerHTML +=
+	    ['<div class="col s12 m3">',
+	     '<div class="card">',
+	     '<div class="card-image">',
+	     '<img src="', el.picture ,'">',
+	     '<div class="card-action">',
+	     '<a href="#">',array[i].name, '</a>',
+	     '</div>',
+	     '</div>',
+	     '</div>'
+	    ].join("");
+    });
 
-bests();
+}
+if (BEST)
+    bests();
